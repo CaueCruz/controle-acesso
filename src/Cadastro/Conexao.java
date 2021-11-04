7b292ad103f1c7d5c348094e1c82b004d0d6716e
CaueCruz <93743682+CaueCruz@users.noreply.github.com>
2021-11-04 20:07:26 -0300

    Add files via upload

---
+++ src/Cadastro/Conexao.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cadastro;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Conexao {
+    Usuario usuario = new Usuario();
+    java.sql.ResultSet rs;
+    private Connection conexao = null;
+    private Statement comando = null;
+
+        public void Conecta(){    
+            try {     
+                String serverName = "localhost";
+                String mydatabase = "CONTROLE_DE_ACESSO";
+                String username = "root";
+                String password = "220816";          
+                String driverName = "com.mysql.cj.jdbc.Driver";
+                Class.forName(driverName);           
+                String url = "jdbc:mysql://" + serverName + "/" + mydatabase+"?useTimezone=true&serverTimezone=UTC";
+                conexao = DriverManager.getConnection(url,username,password);
+                comando = conexao.createStatement();
+                System.out.println("Conexão com o banco de dados efetuada com sucesso!");
+            }
+            catch (ClassNotFoundException e){
+                        System.out.println("Driver não encontrado!"+e);
+            }
+            catch (Exception e){
+                   System.out.println(" SQL Exception "+ e.getMessage()+" Erro ao conectar o Banco de Dados");
+            }        
+        }
+        //metodos
+        public void InsereDados(String cadastoNome, String cadastroEmail, String cadastroSenha, String cadastroPermissao){
+            usuario.setNome(cadastoNome);
+            usuario.setEmail(cadastroEmail);
+            usuario.setSenha(cadastroSenha);
+            usuario.setPermissao(cadastroPermissao);
+            Inserir();
+        }
+        public void Inserir(){
+        if ((usuario.getNome()!="")&&(usuario.getEmail().length() >1)&&(usuario.getSenha().length()>1)&&(usuario.getPermissao()!="")){
+            try {
+                PreparedStatement PreparedStatement;
+                String sql = "INSERT INTO USUARIOS(Nome, Email, Senha, Permissao)"+ "VALUES(?,?,?,?)"; 
+                PreparedStatement = conexao.prepareStatement(sql);
+                PreparedStatement.setString(1,usuario.getNome()); 
+                PreparedStatement.setString(2,usuario.getEmail());
+                PreparedStatement.setString(3,usuario.getSenha());       
+                PreparedStatement.setString(4,usuario.getPermissao());
+                int intRegistro = PreparedStatement.executeUpdate(); 
+                if (intRegistro !=0){ 
+                    JOptionPane.showMessageDialog(null,"Registro adicionado com sucesso !", "Menagem ao Usuario",JOptionPane.INFORMATION_MESSAGE); 
+                 } 
+                else{ 
+                    JOptionPane.showMessageDialog(null,"Registro nao adicionado !", "Mensagem ao Usuário",JOptionPane.INFORMATION_MESSAGE); 
+                } 
+            }
+            catch (Exception Excecao) {
+                JOptionPane.showMessageDialog(null,"SQLException: " + Excecao.getMessage(),"Erro: Selecao de registro", JOptionPane.INFORMATION_MESSAGE); 
+            }
+        } else {
+            JOptionPane.showMessageDialog(null,"Falta Preencher Campo!\n "+"Aluno: "+usuario.getNome()+"\n"+"RGM: "+ usuario.getEmail()+"\n"+"Curso: "+usuario.getPermissao()); 
+        }
+    }
+    public void Logar(){
+    PreparedStatement pst=null;
+
+    String sql = "SELECT*FROM USUARIOS WHERE Email=?";
+        try {
+            pst = conexao.prepareStatement(sql);
+            pst.setString(1, usuario.getEmail());
+            rs = pst.executeQuery();
+            if(rs.next()){
+                Workspace.telaWorkspace telaworkspace = new Workspace.telaWorkspace();
+                telaworkspace.setVisible(true);
+                dispose();
+            } else{
+                JOptionPane.showMessageDialog(null, "Usuário e ou senha inválidos!");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }       
+
+    }
+
+    private void dispose() {
+        JOptionPane.showMessageDialog(null, "Bem Vindo!");
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
---
+++ src/Cadastro/Usuario.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cadastro;
+
+public class Usuario {
+    public int IDusuario;
+    public String Nome;
+    public  String Email;
+    public String Senha;
+    public String Permissao;
+
+    public int getIDusuario() {
+        return IDusuario;
+    }
+
+    public void setIDusuario(int IDusuario) {
+        this.IDusuario = IDusuario;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+
+    public String getPermissao() {
+        return Permissao;
+    }
+
+    public void setPermissao(String Permissao) {
+        this.Permissao = Permissao;
+    }
+}
